8ca4be24d36acc716022ad4aa4af6fe35e77a059
Paolo Savoldi <paooolino@gmail.com>
2016-09-06 16:34:23 +0200

    api crud

--- src/App.php
+++ src/App.php
@@ -17,8 +17,23 @@ class App {
 	}
 	
 	private function setRoutes() {
-		$this->slim->get('/users', '\SlimRest\Models\Users:getList');
-		$this->slim->get('/countries', '\SlimRest\Models\Countries:getList');
+		// Create
+		$this->slim->post('/users', '\SlimRest\Models\Users:newItem');
+		$this->slim->post('/countries', '\SlimRest\Models\Countries:newItem');
+		
+		// Read
+		$this->slim->get('/users', '\SlimRest\Models\Users:getItems');
+		$this->slim->get('/countries', '\SlimRest\Models\Countries:getItems');
+		$this->slim->get('/users/{id}', '\SlimRest\Models\Users:getItem');
+		$this->slim->get('/countries/{id}', '\SlimRest\Models\Countries:getItem');
+		
+		// Update
+		$this->slim->put('/users/{id}', '\SlimRest\Models\Users:updateItem');
+		$this->slim->put('/countries/{id}', '\SlimRest\Models\Countries:updateItem');
+		
+		// Delete
+		$this->slim->delete('/users/{id}', '\SlimRest\Models\Users:deleteItem');
+		$this->slim->delete('/countries/{id}', '\SlimRest\Models\Countries:deleteItem');
 	}
 	
 	public function run() {
--- src/Models/Table.php
+++ src/Models/Table.php
@@ -4,14 +4,71 @@ namespace SlimRest\Models;
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use RedBeanPHP\R as R;
 
 class Table {
 	protected $tablename;
 	
-	public function getList(Request $request, Response $response) {
-		$data = array($this->tablename, 1, 2, 3);
-		$response->getBody()->write(json_encode($data));
+	public function newItem(Request $request, Response $response) {
+		$data = json_decode($request->getBody());
+		
+		$item = R::dispense($this->tablename);
+		foreach($data as $k => $v) {
+			$item->$k = (string)$v;
+		}
+		$id = R::store($item); 
 
-		return $response;
+    return $response->withJson(R::exportAll($item));
 	}
+	
+	public function getItems(Request $request, Response $response) {
+		$items = R::find($this->tablename); 
+		return $response->withJson(R::exportAll($items));
+	}
+	
+	public function getItem(Request $request, Response $response, $args) {
+		$item = R::findOne($this->tablename, 'id=?', array($args["id"]));
+		if($item) {
+			return $response->withJson(R::exportAll($item));
+		} else {
+			return $response
+				->withStatus(404)
+				->withHeader('Content-Type', 'text/html')
+				->write('Not found');
+		}
+	}
+	
+	public function updateItem(Request $request, Response $response, $args) {
+		$data = json_decode($request->getBody());
+		
+		$item = R::findOne($this->tablename, 'id=?', array($args["id"]));
+		
+		if($item) {
+			foreach($data as $k => $v) {
+				$item->$k = (string)$v;
+			}
+			R::store($item); 
+
+			return $response->withJson(R::exportAll($item));
+		} else {
+			return $response
+				->withStatus(404)
+				->withHeader('Content-Type', 'text/html')
+				->write('Not found');
+		}
+	}
+	
+	public function deleteItem(Request $request, Response $response, $args) {
+		$item = R::findOne($this->tablename, 'id=?', array($args["id"]));
+		if($item) {
+			R::trash($item);
+			return $response->withStatus(204);
+		} else {
+			return $response
+				->withStatus(404)
+				->withHeader('Content-Type', 'text/html')
+				->write('Not found');
+		}
+	}
+	
 }
